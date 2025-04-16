7a0459f7076ced5a78c32979a91bcf3bb0469d3f
MichelleF <deshanfernando67@gmail.com>
2025-04-16 13:52:03 +0530

    ..

---
+++ EventBookingApi/Models/Event.cs
@@ -0,0 +1,11 @@
+namespace EventBookingApi.Models;
+
+public class Event
+{
+    public string Id { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public string Location { get; set; } = string.Empty;
+    public string VendorId { get; set; } = string.Empty;
+}
--- EventBookingApi/obj/Debug/net9.0/EventBookingApi.AssemblyInfo.cs
+++ EventBookingApi/obj/Debug/net9.0/EventBookingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventBookingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b48b720c8e7daf45712299e84e278377fe500d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73c2d53945099328999d4737364a5fbb5cc524d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventBookingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventBookingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
