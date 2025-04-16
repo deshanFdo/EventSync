e445a2ec8fe1a013fd39230af04c3f6d1c362726
deshanFdo <62216403+deshanFdo@users.noreply.github.com>
2025-04-16 13:29:27 +0530

    Delete EventBookingApi directory

--- EventBookingApi/Models/Booking.cs
+++
@@ -1,9 +0,0 @@
-namespace EventBookingApi.Models;
-
-public class Booking
-{
-    public string Id { get; set; } = string.Empty;
-    public string UserId { get; set; } = string.Empty;
-    public string EventId { get; set; } = string.Empty;
-    public DateTime BookedAt { get; set; }
-}
--- EventBookingApi/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace EventBookingApi.Models;
-
-public class User
-{
-    public string Id { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string Role { get; set; } = string.Empty;
-}
